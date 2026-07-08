590027ee9555258403564f813de679978228d761
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2026-07-08 17:35:57 +0200

    fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,30 @@
-let
-  gtk = pkgs.callPackage ./pkgs/gtk.nix {};
-  kvantum = pkgs.callPackage ./pkgs/kvantum.nix {};
-in {
-  inherit gtk kvantum;
+{
+  description = "Tokyo Night theme for GTK and Kvantum inspired by Colloid";
 
-  default = pkgs.symlinkJoin {
-    name = "tokyo-night-colloid";
-    paths = [ gtk kvantum ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
+
+  outputs = { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+    in {
+      packages = lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          gtk = pkgs.callPackage ./pkgs/gtk.nix { };
+          kvantum = pkgs.callPackage ./pkgs/kvantum.nix { };
+        in {
+          inherit gtk kvantum;
+
+          default = pkgs.symlinkJoin {
+            name = "tokyo-night-colloid";
+            paths = [ gtk kvantum ];
+          };
+        });
+    };
 }
